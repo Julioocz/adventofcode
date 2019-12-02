ffc36851645f9c249269a420e68c80ec296d35c9
Julio Navarro <jnavarro@able.co>
2019-12-02 08:15:33 -0500

    Day 2 Solution

---
+++ day2.js
@@ -0,0 +1,59 @@
+const createOperation = combiner => (initialIndex, intcode) => {
+  const position1 = intcode[initialIndex];
+  const position2 = intcode[initialIndex + 1];
+  const resultPosition = intcode[initialIndex + 2];
+  intcode[resultPosition] = combiner(intcode[position1], intcode[position2]);
+};
+
+const sum = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const sumOperation = createOperation(sum);
+const multiplyOperation = createOperation(multiply);
+
+const parseInput = input => input.split(",").map(val => parseInt(val, 10));
+
+function computer(intcode) {
+  index = 0;
+  while (index < intcode.length) {
+    let optCode = intcode[index];
+    switch (optCode) {
+      case 1:
+        sumOperation(index + 1, intcode);
+        index += 4;
+        continue;
+      case 2:
+        multiplyOperation(index + 1, intcode);
+        index += 4;
+        continue;
+      case 99:
+        return intcode;
+      default:
+        throw new Error();
+    }
+  }
+  return intcode;
+}
+
+const INPUT =
+  "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,9,19,23,1,6,23,27,1,10,27,31,1,5,31,35,2,6,35,39,1,5,39,43,1,5,43,47,2,47,6,51,1,51,5,55,1,13,55,59,2,9,59,63,1,5,63,67,2,67,9,71,1,5,71,75,2,10,75,79,1,6,79,83,1,13,83,87,1,10,87,91,1,91,5,95,2,95,10,99,2,9,99,103,1,103,6,107,1,107,10,111,2,111,10,115,1,115,6,119,2,119,9,123,1,123,6,127,2,127,10,131,1,131,6,135,2,6,135,139,1,139,5,143,1,9,143,147,1,13,147,151,1,2,151,155,1,10,155,0,99,2,14,0,0";
+
+// First part
+const input = parseInput(INPUT);
+input[1] = 12;
+input[2] = 2;
+console.log("First Part:", computer(input)[0]);
+
+// Second part
+// Finding rersult for second part using brute force
+const initial = parseInput(INPUT);
+for (let i = 0; i <= 99; i++) {
+  for (let j = 0; j <= 99; j++) {
+    const input = [...initial];
+    input[1] = i;
+    input[2] = j;
+    const result = computer(input)[0];
+    if (result === 19690720) {
+      console.log("Second Part:", i, j);
+    }
+  }
+}
